cb28e8db160d25348ec497f3db85a62c534d5ae7
ByteMe7 <peanut15train@gmail.com>
2025-01-16 17:01:53 -0700

    first commit

---
+++ src/main/java/net/funkystudios/funkyweapons/FunkyWeaponsClient.java
@@ -0,0 +1,10 @@
+package net.funkystudios.funkyweapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunkyWeaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/funkystudios/funkyweapons/FunkyWeaponsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.funkystudios.funkyweapons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FunkyWeaponsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
